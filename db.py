9a911ebcec280f36d4488cc4e3932df29948f12d
vivianaperez <viviana.perez@uala.com.ar>
2021-09-28 22:33:50 -0300

    24-9

--- db.py
+++ db.py
@@ -1,25 +1,13 @@
-import pyodbc
-'''
-Otros posibles nombres de servidores pueden ser
-servidor = 'localhost\sqlexpress' # para casos específicos
-servidor = 'miservidor,puerto' # para especificar un puerto alterno
-'''
-servidor = 'puerto:servidor.base.sql'  # Nombre del servidor SQL con el cual se hará la conexión
-bddatos = 'mibdd'  # Nombre de la base de datos SQL
-usuario = 'username' # Nombre del usuario de SQL
-clave = 'clave'  # Contraseña del usuario de SQL
-conn = pyodbc.connect('DRIVER={Controlador ODBC para el servidor SQL};
-                       SERVER='+servidor+';
-                       DATABASE='+bddatos+';
-                       UID='+usuario+';
-                       PWD='+clave)
-'''
-pyodbc.connect() inicia la conexión con el servidor SQL
-tomando como argumentos, en orden,:
-- El nombre del controlador SQL
-- El servidor al cual se hará la conexión
-- El nombre de la Base de datos que será consultada
-- El nombre de usuario que tiene acceso a la base de datos
-- La contraseña del usuario que accede a la base de datos
-'''
-cursor = conn.cursor() # se crea el cursor para la conexión
+import cx_Oracle
+
+connection = cx_Oracle.connect(user="viviana_perez", password="qX%NVLrF0pQa",
+                               dsn="localhost/rds.test.integrator.co.ua.la")
+
+cursor = connection.cursor()
+cursor.execute("""
+        SELECT *
+        FROM SYSREPODB.Z_ACH_OPERATION zao
+        WHERE DATETIME > TO_DATE('28/09/2021','DD/MM/YYYY')""",
+               )
+for operation_id, datetime, msg_in, msg_out in cursor:
+    print("Values:", operation_id, datetime, msg_in, msg_out)
