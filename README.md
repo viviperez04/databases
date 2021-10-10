c54ef1471b2fd4c75dcf55a65909685149f2f886
vivianaperez <viviana.perez@uala.com.ar>
2021-10-09 23:00:13 -0300

    9-10

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ PTS
 SYSREPODB.Z_ACH_OPERATION zao
 cx_oracle
 instantclient_19_8
-instantclient_18_1
+
--- db.py
+++ db.py
@@ -1,14 +1,31 @@
-
 import cx_Oracle
+import json
+from dtos import mensaje
 
-connection = cx_Oracle.connect(user="viviana_perez", password="qX%NVLrF0pQa",
-                               dsn="localhost/rds.test.integrator.co.ua.la")
 
-cursor = connection.cursor()
-cursor.execute("""
-        SELECT *
-        FROM SYSREPODB.Z_ACH_OPERATION zao
-        WHERE DATETIME > TO_DATE('28/09/2021','DD/MM/YYYY')""",
-               )
-for operation_id, datetime, msg_in, msg_out in cursor:
-    print("Values:", operation_id, datetime, msg_in, msg_out)
+def output_type_handler(cursor, name, default_type, size, precision, scale):
+    if default_type == cx_Oracle.DB_TYPE_CLOB:
+        return cursor.var(cx_Oracle.DB_TYPE_LONG, arraysize=cursor.arraysize)
+    if default_type == cx_Oracle.DB_TYPE_BLOB:
+        return cursor.var(cx_Oracle.DB_TYPE_LONG_RAW, arraysize=cursor.arraysize)
+
+
+def main():
+
+    connection = cx_Oracle.connect(user="viviana_perez", password="qX%NVLrF0pQa",
+                                  dsn="rds.test.integrator.co.ua.la:1521/PTSDB")
+    connection.outputtypehandler = output_type_handler
+    cursor = connection.cursor()
+    cursor.execute("""
+           SELECT  OPERATION_ID,msg_in, msg_out
+           FROM SYSREPODB.Z_ACH_OPERATION zao
+           WHERE OPERATION_ID = 1482""",)
+    OPERATION_ID, msg_in, msg_out = cursor.fetchone()
+    jsonIn = json.loads(msg_in)
+    jsonOut = json.loads(msg_out)
+    print("OperationId {0}", OPERATION_ID)
+    print("Msg In Id {0}", jsonIn['headerRQ']['msgId'])
+    print("Msg Error code {0}", jsonOut['errorCode'])
+
+
+main()
